cfb425aea68c7b3e9ffa9422078d5bb73c9ccf23
vivekkumar15 <kmr.vivek15@gmail.com>
2024-04-02 22:40:37 +0530

    add API client and token service

---
+++ src/base_api_client.py
@@ -0,0 +1,124 @@
+import json
+from decimal import Decimal
+from typing import Optional
+
+import requests
+
+from auth_token_service import AuthTokenService
+
+
+class BaseAPIClient(object):
+    def __init__(self, api_key: str, api_url: str, private_key: Optional[bytes] = None, **kwargs):
+        self.api_key = api_key
+        self.api_url = api_url
+        self.headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Api-Key": self.api_key,
+        }
+        self.auth_token_service = AuthTokenService(self.api_key, private_key, **kwargs)
+
+    def get(self, path: str, params: Optional[dict] = None):
+        return self._make_request("GET", url_path=path, params=params)
+
+    def post(self, path: str, data: Optional[dict] = None):
+        return self._make_request("POST", url_path=path, data=data)
+
+    def _make_request(
+        self,
+        method: str,
+        url_path: Optional[str] = None,
+        params: Optional[dict] = None,
+        data: Optional[dict] = None,
+    ):
+        full_url = f"{self.api_url}{url_path}"
+        api_token = self.auth_token_service.generate_auth_token(url_path, data)
+        self.headers["Authorization"] = f"Bearer {api_token}"
+
+        response = None
+        try:
+            if method == "GET":
+                response = requests.get(full_url, headers=self.headers, params=params)
+            elif method == "POST":
+                response = requests.post(
+                    full_url,
+                    headers=self.headers,
+                    params=params,
+                    json=data,
+                )
+            else:
+                raise Exception(f"Invalid method: {method}")
+
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            if response.status_code == 400:
+                raise BadRequestError(
+                    f"400 Bad Request: {e} {response.text}", response_text=response.text
+                )
+            elif response.status_code == 401:
+                raise UnauthorizedError(
+                    f"401 Unauthorized: {e} {response.text}",
+                    response_text=response.text,
+                )
+            elif response.status_code == 403:
+                raise ForbiddenError(
+                    f"403 Forbidden: {e} {response.text}", response_text=response.text
+                )
+            elif response.status_code == 404:
+                raise NotFoundError(
+                    f"404 Not Found: {e} {response.text}", response_text=response.text
+                )
+            elif response.status_code == 429:
+                raise TooManyRequestsError(
+                    f"429 Too Many Requests: {e} {response.text}",
+                    response_text=response.text,
+                )
+
+            elif response.status_code == 500:
+                raise InternalServerError(
+                    f"500 Internal Server Error: {e} {response.text}",
+                    response_text=response.text,
+                )
+            else:
+                raise Exception(f"HTTP Error: {e} {response.text}")
+        except requests.exceptions.RequestException as e:
+            raise Exception(f"Request Exception: {e} {response.text}")
+
+        try:
+            return response.json()
+        except json.decoder.JSONDecodeError:
+            return response.text
+
+
+class BaseAPIException(Exception):
+    def __init__(self, message, response_text=None):
+        super().__init__(message)
+        self.response_text = response_text
+
+
+class BadRequestError(BaseAPIException):
+    pass
+
+
+class UnauthorizedError(BaseAPIException):
+    pass
+
+
+class ForbiddenError(BaseAPIException):
+    pass
+
+
+class NotFoundError(BaseAPIException):
+    pass
+
+
+class InternalServerError(BaseAPIException):
+    pass
+
+
+class ServiceUnavailableError(BaseAPIException):
+    pass
+
+
+class TooManyRequestsError(BaseAPIException):
+    pass
---
+++ src/signature_service.py
@@ -0,0 +1,57 @@
+import base64
+from enum import Enum
+
+import boto3
+from botocore.exceptions import ClientError
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.asymmetric.ec import EllipticCurvePrivateKey
+
+from constants import AWS_REGION, KMS_SIGNING_ALGORITHM
+
+
+class SignatureServiceEnum(Enum):
+    AWS_KMS = "AWS_KMS"
+    PRIVATE_KEY = "PRIVATE_KEY"
+
+
+class BaseSignatureService(object):
+    """Base class for signature."""
+
+    def sign(self, string_to_sign):
+        raise NotImplementedError
+
+    def verify(self, string_to_sign, signature):
+        raise NotImplementedError
+
+
+class PrivateKeySignatureService(BaseSignatureService):
+    def __init__(self, private_key_bytes: bytes):
+        self.private_key: EllipticCurvePrivateKey = serialization.load_pem_private_key(
+            private_key_bytes,
+            password=None,
+            backend=default_backend(),
+        )
+
+    def sign(self, message: bytes):
+        return self.private_key.sign(message, ec.ECDSA(hashes.SHA256()))
+
+
+class KMSSignatureService(BaseSignatureService):
+    def __init__(self, key_id: str):
+        self.kms_client = boto3.client("kms", region_name=AWS_REGION)
+        self.key_id = key_id
+
+    def sign(self, message: bytes):
+        try:
+            response = self.kms_client.sign(
+                KeyId=self.key_id,
+                Message=message,
+                MessageType='RAW',
+                SigningAlgorithm=KMS_SIGNING_ALGORITHM
+            )
+            return response['Signature']
+        except ClientError as e:
+            print(f"An error occurred while signing: {e}")
+            return None
