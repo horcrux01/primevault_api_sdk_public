dee9ff7c80c024fec7b0cd8d02263f53e502a4a0
vivekkumar15 <kmr.vivek15@gmail.com>
2025-02-10 17:28:06 +0530

    txn output type changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="primevault_api_sdk",
-    version="0.1.4",
+    version="0.1.5",
     packages=find_packages(),
     install_requires=[
         "boto3==1.34.84",
