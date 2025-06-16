72464d07c5715ab4e46303c237afbd30294ca9b4
Vivek <42060544+vivekkumar15@users.noreply.github.com>
2025-06-16 15:57:08 +0530

    ALephium contract call (#47)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="primevault_api_sdk",
-    version="1.0.15",
+    version="1.0.17",
     packages=find_packages(),
     install_requires=[
         "boto3==1.34.84",
