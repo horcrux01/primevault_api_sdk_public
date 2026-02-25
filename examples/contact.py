8a4e6c0946bdb0e12537df569486656c03dfee94
tushal-primevault <tushal@primevault.com>
2026-02-25 18:32:49 +0530

    pushed example for the contact create and update

---
+++ examples/contact.py
@@ -0,0 +1,20 @@
+from primevault_python_sdk.api_client import APIClient
+from primevault_python_sdk.types import CreateContactRequest, GetApprovalRequest, UpdateContactRequest
+
+
+def create_contact(api_client: APIClient):
+    contact_request = CreateContactRequest(name="test", address="0xa..",chain='ETHEREUM', assetList=["USDT"])
+    contact_response = api_client.create_contact(contact_request)
+    # action can be approve/reject for the function created below
+    approve_response =api_client.initiate_change_approval_action(GetApprovalRequest(entityId=contact_response.id, action="approve"))
+    return approve_response
+
+def update_contact(api_client: APIClient):
+    contact_request = UpdateContactRequest(id="contact-id",  assetList=["USDC"])
+    contact_response = api_client.update_contact(contact_request)
+    # action can be approve/reject for the function created below
+    approve_response = api_client.initiate_change_approval_action(
+        GetApprovalRequest(entityId=contact_response.id, action="approve"))
+    return approve_response
+
+
