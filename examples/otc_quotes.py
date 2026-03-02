c72e389dc2fbf8e2da2f8ea1d82fcdb058eaa1a3
tushal-primevault <tushal@primevault.com>
2026-03-03 00:06:37 +0530

    remove not required changes

--- examples/otc_quotes.py
+++ examples/otc_quotes.py
@@ -45,36 +45,3 @@ def execute_quote(api_client: APIClient) -> Transaction:
                                       externalId="a9acfd61-2e36-48ab-b709-5c7c4936ea01")
     transaction_response = api_client.create_trade_transaction(transaction_request)
     return transaction_response
-
-
-
-
-
-
-    private_key = '''-----BEGIN EC PRIVATE KEY-----
-MHcCAQEEIBCECHt9gc+HMhBHloWXoTNLw6ZMpUQkgWk1EVgCNYjKoAoGCCqGSM49
-AwEHoUQDQgAE4xtzKI16vOTg6J9eByQTTGAwcHlzkNEZDZSi/bwhAzITuKpwqZ0B
-IxDyYuEvNBG53zrEturrN54lpsVd3Lwc0Q==
------END EC PRIVATE KEY-----'''
-    api_client = APIClient(api_key='284d1372-d0ee-4207-9e41-e49603cc269d',private_key_hex=private_key, api_url='https://api.primevault.com')
-    vault = api_client.get_vault_by_id("a9acfd61-2e36-48ab-b709-5c7c4936ea00")
-    vault_id = vault.id
-    detailed_balances = api_client.get_deposit_address(vault_id, currency="USDC")
-    params = CreateTradeQuoteRequest (
-                vaultId= str(vault.id),
-                fromAsset= "USD",
-                toAsset= "USDT",
-                fromAmount= "2",
-                fromChain = "ETHEREUM",
-                toChain= "ETHEREUM",
-                slippage = "0.1"
-    )
-    a = api_client.get_trade_quote(params)
-    quote_request = a.tradeRequestData
-    quote_response = a.tradeResponseDataList[0]
-    print(a)
-    b = CreateTradeTransactionRequest(vaultId= vault_id, tradeRequestData=  quote_request, tradeResponseData=quote_response , externalId="a9acfd61-2e36-48ab-b709-5c7c4936ea01")
-    print(b)
-
-    c = api_client.create_trade_transaction(b)
-    print(c)
