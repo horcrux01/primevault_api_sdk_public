c9a9d41ce6e0080cf4534ddf176492db70c57893
tushal-primevault <tushal@primevault.com>
2026-03-06 21:05:21 +0530

    example added

---
+++ examples/ramp_example.py
@@ -0,0 +1,25 @@
+from primevault_python_sdk.api_client import APIClient
+from primevault_python_sdk.types import CreateTradeQuoteRequest, CreateRampTransactionRequest, TransactionCategory, \
+    Transaction
+
+
+def create_ramp_transfer(api_client: APIClient) -> Transaction:
+    create_trade_quote_request = CreateTradeQuoteRequest(vaultId="393f359c-6e66-4490-bf1f-5a4ec44f49d6",fromAsset="USD",toAsset="USDC", fromAmount="100", category="ON_RAMP", paymentMethod="US_ACH", toChain="POLYGON")
+    trade_response = api_client.get_trade_quote(create_trade_quote_request)
+    trade_request_data = trade_response.tradeRequestData
+    trade_routes = trade_response.tradeResponseDataList or []
+    trade_response_data = trade_routes[0]
+    vault_id = str(create_trade_quote_request.vaultId)
+    on_ramp_request = CreateRampTransactionRequest(
+        vaultId=vault_id,
+        category=TransactionCategory.ON_RAMP.value,
+        tradeRequestData=trade_request_data,
+        tradeResponseData=trade_response_data,
+        externalId=f"on-ramp-ext-8",
+        operationMessage="ON_RAMP test",
+        memo="on ramp test",
+        paymentMethod="US_ACH",
+        toBlockChain="POLYGON",
+    )
+    on_ramp_transaction_response = api_client.create_ramp_transaction(on_ramp_request)
+    return on_ramp_transaction_response
